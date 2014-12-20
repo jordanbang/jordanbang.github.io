c76300b64d1c2751fd8bcabedfd5d520fc052297
Jordan Bangia <jordan.bangia@gmail.com>
2014-12-19 21:46:56 -0500

    Switched to page switching being done by javascript

--- custom.js
+++ custom.js
@@ -1,3 +1,31 @@
-$(documnet).ready(function() {
-	$("")
-})
+$(document).ready(function() {
+	$('#school_content').hide();
+	$('#project_content').hide();
+	$('#school').removeClass('active');
+	$('#projects').removeClass('active');
+
+
+	$('#home').click(function() {
+		$('#school_content').hide();
+		$('#project_content').hide();
+		$('#home').addClass('active');
+		$('#school').removeClass('active');
+		$('#projects').removeClass('active');
+	});
+
+	$('#school').click(function() {
+		$('#school_content').show();
+		$('#project_content').hide();
+		$('#home').removeClass('active');
+		$('#school').addClass('active');
+		$('#projects').removeClass('active');
+	});
+
+	$('#projects').click(function() {
+		$('#school_content').hide();
+		$('#project_content').show();
+		$('#home').removeClass('active');
+		$('#school').removeClass('active');
+		$('#projects').addClass('active');
+	});
+});
